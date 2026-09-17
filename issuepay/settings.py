3dbfff855fae306ae0da0b9d7892c6a01769c544
Otibra <otienobrian718@gmail.com>
2026-09-17 05:00:02 -0700

    Move Django secret key to environment

--- issuepay/settings.py
+++ issuepay/settings.py
@@ -10,17 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6v9cbc8rh^@ef!^(!y(yd!4tv@hci%0h(veulm9&izco(dn3al'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+if not SECRET_KEY:
+    raise RuntimeError('DJANGO_SECRET_KEY is not set.')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
