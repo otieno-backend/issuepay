a27e87d24b12a2a87cc8c1a4a828813a3ba66ffe
Otibra <otienobrian718@gmail.com>
2026-09-19 05:50:05 -0700

    Register custom user in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("IssuePay Information", {
+            "fields": ("role",),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("IssuePay Information", {
+            "fields": ("email", "role"),
+        }),
+    )
+
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "is_staff",
+        "is_active",
+    )
+
+    list_filter = (
+        "role",
+        "is_staff",
+        "is_active",
+    )
